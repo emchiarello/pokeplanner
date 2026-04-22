3622146c6b3cc74d5928cd7110eedaf229b3999b
Hugo Gonçalves <goncahug@students.zhaw.ch>
2026-04-22 17:16:33 +0200

    Add more ESLint rules, introduce Prettier & pre-commit hooks

--- planner/eslint.config.js
+++ planner/eslint.config.js
@@ -3,6 +3,8 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import eslintPluginPrettier from 'eslint-plugin-prettier'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
@@ -14,10 +16,41 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      eslintConfigPrettier,
+      {
+        plugins: { prettier: eslintPluginPrettier },
+        rules: {
+          'prettier/prettier': 'error',
+        },
+      },
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    rules: {
+      // TS
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        { prefer: 'type-imports', fixStyle: 'inline-type-imports' },
+      ],
+      '@typescript-eslint/no-shadow': 'error',
+      '@typescript-eslint/no-non-null-assertion': 'warn',
+
+      // React
+      'react-hooks/exhaustive-deps': 'warn',
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+
+      // General
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'no-restricted-imports': [
+        'error',
+        {
+          patterns: ['../src/*', './src/*'],
+        },
+      ],
+      'prefer-const': 'error',
+      'no-var': 'error',
     },
   },
 ])
