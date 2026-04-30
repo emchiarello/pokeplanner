4f1878dac8db9bebf97bccdcea6939993ac6d628
Emanuele Chiarello <chiarema@students.zhaw.ch>
2026-04-30 17:30:43 +0200

    run eslint

--- planner/components/Button.tsx
+++ planner/components/Button.tsx
@@ -1,15 +1,27 @@
-import type {ReactNode} from "react";
+import type { ReactNode } from 'react';
 
-export function Button({ variant, onClick, children}: { variant: string; onClick?: () => void; children: ReactNode; }) {
+export function Button({
+  variant,
+  onClick,
+  children,
+}: {
+  variant: string;
+  onClick?: () => void;
+  children: ReactNode;
+}) {
   const buttonStyle: Record<string, string> = {
-    primary: "bg-black dark:bg-white border text-white dark:text-black cursor-pointer",
-    primary_disabled: "bg-gray-500 border border-gray-500",
-    secondary: "border cursor-pointer",
-  }
+    primary: 'bg-black dark:bg-white border text-white dark:text-black cursor-pointer',
+    primary_disabled: 'bg-gray-500 border border-gray-500',
+    secondary: 'border cursor-pointer',
+  };
 
   return (
-    <button type="button" onClick={onClick} className={`w-full px-4 py-2 rounded-xl ${buttonStyle[variant]}`}>
+    <button
+      type="button"
+      onClick={onClick}
+      className={`w-full px-4 py-2 rounded-xl ${buttonStyle[variant]}`}
+    >
       {children}
     </button>
-  )
+  );
 }
